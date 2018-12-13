d2090b15768057c04dd63877f0e7adf6b6ed5ffc
aboud20-meet <aboud20@meet.mit.edu>
2018-12-13 18:46:47 +0200

    apis

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
+import requests, json
+app=Flask(__name__)
 
 @app.route('/')
 def home():
@@ -16,7 +17,26 @@ def study_image():
 
     # YOUR CODE HERE!
     
-    return render_template('home.html', results="No results yet :(")
+    headers = {'Authorization': 'Key 62c59b4eb52547238acda6225d9bd908'}
+    api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+    data ={"inputs": [
+      {
+        "data": {
+          "image": {
+            "url_": image_url
+          }
+        }
+      }
+    ]}
+
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
+    response_dict = json.loads(response.content)
+    response_dict["outputs"][0]["data"]["concepts"]
+
+
+
+
+    return render_template('home.html', results=response_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
